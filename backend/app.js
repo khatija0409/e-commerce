df5a3df27f9706a30ae906a522c7fdb4f5680e68
khatija0409 <khatija0409@gmail.com>
2024-01-03 11:59:55 +0530

    Create health api checkpoint

--- backend/app.js
+++ backend/app.js
@@ -41,6 +41,10 @@ app.get("*",(req,res)=>{
   res.sendFile(path.resolve(__dirname,"../frontend/build/index.html"));
 })
 
+app.get("/health", (req,res)=>{
+  res.json("Healthy APi Checkpoint")
+})
+
 // middleware for error
 app.use(errorMiddleware);
 
